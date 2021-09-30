5430ddb311074a974f193e2f577a98164fdc11a8
nesseri <eric.ness@chrobinson.com>
2021-09-30 11:30:01 -0500

    Clean up colorize_status

--- ch11-cli/uptimer_ecn/uptimer_ecn/uptimer_ecn.py
+++ ch11-cli/uptimer_ecn/uptimer_ecn/uptimer_ecn.py
@@ -7,29 +7,26 @@ def check_url(url):
         response = requests.head(url)
     except requests.exceptions.ConnectionError:
         click.echo(f"ConnectionError: Can't reach {url} URL!")
-        return None
+        return -1
     return response.status_code
 
 
 def colorize_status(url, status):
-    if status // 100 == 2:
-        click.secho(f"{url} -> {status}", fg="green")
-    elif status // 100 == 3:
-        click.secho(f"{url} -> {status}", fg="yellow")
-    elif status // 100 == 4:
-        click.secho(f"{url} -> {status}", fg="bright_red")
-    elif status // 100 == 5:
-        click.secho(f"{url} -> {status}", fg="red")
-    else:
-        click.secho(f"{url} -> {status}", fg="magenta")
+    colors = {
+        2: "green",
+        3: "yellow",
+        4: "bright_red",
+        5: "red",
+        -1: "magenta",
+    }
+    click.secho(f"{url} -> {status}", fg=colors.get(status // 100))
 
 
 @click.command()
 @click.argument("url")
 def check(url):
     status_code = check_url(url)
-    if status_code:
-        colorize_status(url, status_code)
+    colorize_status(url, status_code)
 
 
 if __name__ == '__main__':
