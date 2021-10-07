0d1c36ced9097625642b7c7da246ac871bb5e590
nesseri <eric.ness@chrobinson.com>
2021-10-07 11:03:22 -0500

    Add daemon flag and multiple urls

--- ch11-cli/uptimer_ecn/uptimer_ecn/uptimer_ecn.py
+++ ch11-cli/uptimer_ecn/uptimer_ecn/uptimer_ecn.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import click
 import requests
 
@@ -23,10 +25,17 @@ def colorize_status(url, status):
 
 
 @click.command()
-@click.argument("url")
-def check(url):
-    status_code = check_url(url)
-    colorize_status(url, status_code)
+@click.argument("urls", nargs=-1)
+@click.option("--daemon", "-d", default=False, is_flag=True)
+def check(urls, daemon):
+    while True:
+        for url in urls:
+            status_code = check_url(url)
+            colorize_status(url, status_code)
+            if not daemon:
+                break
+            sleep(1)
+
 
 
 if __name__ == '__main__':
